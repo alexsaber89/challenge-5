174868c3120ad1305631b2a17b3c6ac6eaa154eb
Alex Saber <Alex Saber>
2016-08-12 21:57:19 -0500

    declared runFib(); challenge complete

--- app.js
+++ app.js
@@ -1,50 +1,14 @@
 //Write a Fibonacci number generator that outputs the numbers in the series that are less than 500.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function fibbo(n) {
-//   return Array.apply(0, Array(n)).
-//   reduce(function(x, y, z){return x.concat((z < 2) ? z : x[z - 1] + x[z - 2]); }, []);
-// }
-
-
-
-
-// var fibArray = [1,1];
-// var newArray = fibArray.filter(fibArray.reduce(previousValue,currentValue =>);
-// for (var i = 0; i < 500; i++) {
-// }
-
-// var filtered = [1,1].filter(element => value < 500);
-// console.log(filtered);
-
-// // filtered is [12, 130, 44]
-// fibArray.reduce(function(1,1) {
-//   fibArray.push(previousValue + currentValue);
-// });
+function runFib() {
+  var fibArray = [0,1];
+  for (var i = 0;; i++) {
+    var newNum = fibArray[i] + fibArray[i+1];
+    if (newNum < 500) {
+      fibArray.push(newNum);
+    } else {
+      break;
+    }
+  }
+  console.log(fibArray);
+}
+runFib();
