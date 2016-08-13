b06fe201055483c99224b5e18812349527f43a87
Alex Saber <Alex Saber>
2016-08-12 21:05:54 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+//Write a Fibonacci number generator that outputs the numbers in the series that are less than 500.
+
+var body = document.getElementById("main-body");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function fibbo(n) {
+//   return Array.apply(0, Array(n)).
+//   reduce(function(x, y, z){return x.concat((z < 2) ? z : x[z - 1] + x[z - 2]); }, []);
+// }
+
+
+
+
+// var fibArray = [1,1];
+// var newArray = fibArray.filter(fibArray.reduce(previousValue,currentValue =>);
+// for (var i = 0; i < 500; i++) {
+// }
+
+// var filtered = [1,1].filter(element => value < 500);
+// console.log(filtered);
+
+// // filtered is [12, 130, 44]
+// fibArray.reduce(function(1,1) {
+//   fibArray.push(previousValue + currentValue);
+// });
